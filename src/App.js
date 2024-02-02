6b68b96a12e9413c70e1e87af4068755a654bc55
Divya <divyaratna1234@gmail.com>
2024-02-02 21:44:53 +0530

    Added AboutUs Page | TextArea to type | Added buttons for different functions | Added Dark Mode | added Alert Functionalities

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import About from './components/About';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+import Alert from './components/Alert';
 
 function App() {
+  const [mode, setMode] = useState('light');
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type
+    });
+    setTimeout(() => {
+      setAlert(null)
+    }, 2000);
+  };
+
+  const toggleMode = () => {
+    if (mode === 'light') {
+      setMode('dark')
+      document.body.style.backgroundColor = 'white';
+      showAlert("Light Mode Enabled", "success")
+    }
+    else {
+      setMode('light')
+      document.body.style.backgroundColor = 'dark';
+      showAlert("Dark Mode Enabled", "success");
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div style={{ minHeight: '100%', backgroundColor: mode === 'dark' ? '#121212' : 'white' }}>
+        <Navbar title="My TextUtils" mode={mode} toggleMode={toggleMode} />
+        <Alert alert={alert} />
+        <About mode={mode} />
+        <TextForm showAlert={showAlert} mode={mode} heading="Enter Text" />
+      </div>
+    </>
   );
 }
 
---
+++ src/components/TextForm.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react'
+
+
+export default function TextForm(props) {
+    const handleOnChange = (event) => {
+        setText(event.target.value);
+    }
+    const handleUpClick = () => {
+        let newText = text.toUpperCase();
+        setText(newText);
+        props.showAlert("Converted to UpperCase!", "success");
+    }
+    const handleLoClick = () => {
+        let newText = text.toLowerCase();
+        setText(newText);
+        props.showAlert("Converted to LowerCase!", "success");
+    }
+    const handleCopy = () => {
+        navigator.clipboard.writeText(text);
+        props.showAlert("Copied to Clipboard!", "success");
+    }
+    const handleClear = () => {
+        let newtext = '';
+        setText(newtext);
+        props.showAlert("Text Cleared!", "success");
+    }
+    const [text, setText] = useState('');
+    return (
+        <>
+            <div className="container" data-bs-theme={props.mode}>
+                <div className="mb-3">
+                    <h1 style={{ color: props.mode === 'dark' ? 'white' : 'black' }}>{props.heading}</h1>
+                    <textarea className="form-control" onChange={handleOnChange} value={text} id="exampleFormControlTextarea1" rows="20"></textarea>
+
+                    <button disabled={text.length === 0} className="btn btn-primary mx-1 my-2" type="button" onClick={handleUpClick}>Convert to UpperCase</button>
+                    <button disabled={text.length === 0} className="btn btn-primary mx-1 my-2" type="button" onClick={handleLoClick}>Convert to LowerCase</button>
+                    <button disabled={text.length === 0} className="btn btn-primary mx-1 my-2" type="button" onClick={handleCopy}>Copy to CLipBoard</button>
+                    <button disabled={text.length === 0} className="btn btn-primary mx-1 my-2" type="button" onClick={handleClear}>Clear All</button>
+                </div>
+            </div>
+        </>
+    )
+}
